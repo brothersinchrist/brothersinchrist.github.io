99d05edf5b5073aeba16986dbb649377918028e3
thomaswparks <thomaswparks@users.noreply.github.com>
2026-01-17 03:40:54 +0000

    deploy: brothersinchrist/menofgod-content@583f8740b9514073b9465065e36385fc1ab41729

--- tmp/jekyll-remote-theme-20260116-4018520-mdkys5/assets/js/lunr/lunr-store.js
+++ tmp/jekyll-remote-theme-20260116-4018520-mdkys5/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Blog",
-        "excerpt":"Faithful Stewards of Gods Grace January 13, 2026 • DeShun Bland The Lord Jesus is full of Grace and truth, and by his blood we have been made new so let us buy faith in Christ walk in the newness that he has given us in obedience Read More Trust...","categories": [],
+        "excerpt":"Faithful Stewards of Gods Grace January 13, 2026 • DeShun Bland The Lord Jesus is full of Grace and truth, and by his blood we have been made new so let us buy faith in Christ walk in the newness that he has given us in obedience Trust and Obey...","categories": [],
         "tags": [],
         "url": "/menofgod-site/blog/",
         "teaser": null
