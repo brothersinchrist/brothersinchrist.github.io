201a7d1a487760759bc2df444ba14c07cd188a4f
thomaswparks <thomaswparks@users.noreply.github.com>
2026-01-20 04:30:02 +0000

    deploy: brothersinchrist/menofgod-content@ccd8b71592d5525d6818af8090eab58eee944c73

--- tmp/jekyll-remote-theme-20260116-4020730-eqw0ud/assets/js/lunr/lunr-store.js
+++ tmp/jekyll-remote-theme-20260116-4020730-eqw0ud/assets/js/lunr/lunr-store.js
@@ -2598,7 +2598,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Meet the Hosts",
-        "excerpt":"DeShun Husband, father, and co‑host and founder of Men of God. A devoted husband and father of five, raising four sons and a daughter. He and his family are active in their church, and his faith guides how he leads at home and serves his community. Grounded in God’s truth,...","categories": [],
+        "excerpt":"LIQUID WORKS DeShun Husband, father, and co‑host and founder of Men of God. A devoted husband and father of five, raising four sons and a daughter. He and his family are active in their church, and his faith guides how he leads at home and serves his community. Grounded in...","categories": [],
         "tags": [],
         "url": "/menofgod-site/webparts_about/03-hosts.html",
         "teaser": null
